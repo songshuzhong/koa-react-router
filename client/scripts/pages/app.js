a437f3b8bdcb92a8ae95b66918b89b79f21cc4d0
songshuzhong <18346667355@163.com>
2018-03-20 21:17:40 +0800

    feat: add react-router|nprogress

--- client/scripts/pages/app.js
+++ client/scripts/pages/app.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+
+import { getDataSource } from '../utilities/dataSource';
+
 import logo from '../../media/logo.svg';
 import '../../styles/app.css';
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = { val: 0 };
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+/*    let p1 = fetch( '/common-navaid/data' );
+    let p2 = fetch( '/common-navaid/data' );
+
+    let faster = Promise.race( [ p1, p2 ] ).then( res => console.log( res ) );
+    console.log( faster );*/
+
+    getDataSource( '/common-navaid/data', ( data ) => console.log( data ) );
+  }
+
+  componentWillReceiveProps( nextProps ) {
+
+  }
+
+  shouldComponentUpdate() {
+
+  }
+
+  componentWillUpdate() {
+
+  }
+
+  componentDidUpdate() {
+
+  }
+
   render() {
     return (
       <div className="App">
